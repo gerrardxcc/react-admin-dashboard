37c49474180c8dbcf374636396be36f5c6bf3d8c
Gerrard Xiong <chengcheng.xiong@my.jcu.edu.au>
2023-07-15 23:38:35 +1000

    React Menu Design

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,6 +1,11 @@
 import './footer.scss';
 
 const Footer = () => {
-  return <div className='footer'>Footer</div>;
+  return (
+    <div className='footer'>
+      <span>React Admin</span>
+      <span>© Gerrard Admin Dashboard</span>
+    </div>
+  );
 };
 export default Footer;
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,6 +1,30 @@
 import './navbar.scss';
 
 const Navbar = () => {
-  return <div className='navbar'>Navbar</div>;
+  return (
+    <div className='navbar'>
+      <div className='logo'>
+        <img src='logo.svg' alt='' />
+        <span>React Admin</span>
+      </div>
+      <div className='icons'>
+        <img src='/search.svg' alt='icon' />
+        <img src='/app.svg' alt='icon' />
+        <img src='/expand.svg' alt='icon' />
+        <div className='notification'>
+          <img src='/notifications.svg' alt='' />
+          <span>1</span>
+        </div>
+        <div className='user'>
+          <img
+            src='https://avatars.githubusercontent.com/u/36846475?v=4'
+            alt=''
+          />
+          <span>Gerrard</span>
+        </div>
+        <img src='/settings.svg' alt='icon' />
+      </div>
+    </div>
+  );
 };
 export default Navbar;
